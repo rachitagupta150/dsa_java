1555ecbfc1d2c6e95e32916a6d84775f506fdc88
rachitagupta150 <137597111+rachitagupta150@users.noreply.github.com>
2024-06-25 22:39:24 +0530

    11 questions

---
+++ hackerrank_string_introduction.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class hackerrank_string_introduction{
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        System.out.println(A.length()+B.length());
+        int x =A.compareTo(B);
+        if(x>0){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+        System.out.println(A.substring(0,1).toUpperCase()+A.substring(1)+" "+B.substring(0,1).toUpperCase()+B.substring(1));
+    }
+}
+
+
+
