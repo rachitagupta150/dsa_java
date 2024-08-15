81504cc6f188c0ec97cf3136cac58629adadad30
rachitagupta150 <rachitagupta150@gmail.com>
2024-08-15 16:26:32 +0530

    implementation of queue

---
+++ implementation_of_queue.java
@@ -0,0 +1,212 @@
+//implementation of queue using array
+// public class implementation_of_queue {
+//     static class Queue{
+//         static int arr[];
+//         static int size;
+//         static int rear;
+//         Queue(int n){
+//             arr=new int[n];
+//             size=n;
+//             rear=-1;
+//         }
+//         public static boolean isEmpty(){
+//             return rear==-1;
+//         }
+//         public static void add(int data){
+//             if(rear==size-1){
+//                 System.out.println("queue is full");
+//                 return;
+//             }
+//             rear=rear+1;
+//             arr[rear]=data;
+//         }
+//         public static int remove(){
+//             if(isEmpty()){
+//                 System.out.println("empty queue");
+//                 return -1;
+//             }
+//             int front=arr[0];
+//             for(int i=0;i<rear;i++){
+//                 arr[i]=arr[i+1];
+//             }
+//             rear =rear-1;
+//             return front;
+            
+//         }
+//         public static int peek(){
+//             if(isEmpty()){
+//                 System.out.println("empty queue");
+//                 return -1;
+//             }
+//             return arr[0];
+//         }
+//     }
+//     public static void main(String[] args) {
+//         Queue q=new Queue(5);
+//         q.add(1);
+//         q.add(2);
+//         q.add(3);
+//         while(!q.isEmpty()){
+//             System.out.println(q.peek());
+//             q.remove();
+//         }
+//     }
+// }
+
+
+
+
+// implementation of queue using linkedlist
+// public class implementation_of_queue {
+//     static class Node{
+//         int data;
+//         Node next;
+//         Node(int data){
+//             this.data=data;
+//             this.next=null;
+//         }
+//     }
+//     static class Queue{
+//         static Node head=null;
+//         static Node tail=null;
+//         public static boolean isEmpty(){
+//             return head==null & tail==null;
+//         }
+//         public static void add(int data){
+//             Node newnode=new Node(data);
+//             if(head==null){
+//                 head=tail=newnode;
+//                 return;
+//             }
+//             tail.next=newnode;
+//             tail=newnode;
+//         }
+//         public static int remove(){
+//             if(isEmpty()){
+//                 System.out.println("empty queue");
+//                 return -1;
+//             }
+//             int front=head.data;
+//             if(head==tail){
+//                 head=tail=null;
+//             }
+//             else{
+//                 head=head.next;
+//             }
+//             return front;
+            
+//         }
+//         public static int peek(){
+//             if(isEmpty()){
+//                 System.out.println("empty queue");
+//                 return -1;
+//             }
+//             return head.data;
+//         }
+//     }
+//     public static void main(String[] args) {
+//         Queue q=new Queue();
+//         q.add(1);
+//         q.add(2);
+//         q.add(3);
+//         while(!q.isEmpty()){
+//             System.out.println(q.peek());
+//             q.remove();
+//         }
+//     }
+// }
+
+
+
+// implementation of queue using jcf
+import java.util.*;
+public class implementation_of_queue {
+   
+    
+    public static void main(String[] args) {
+        // Queue <Integer> q=new LinkedList<>();
+        Queue <Integer> q=new ArrayDeque<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
+
+
+
+
+
+
+// //implementation of circular queue using array
+// public class ImplementationOfCircularQueue {
+//     static class Queue {
+//         private int[] arr;
+//         private int size;
+//         private int rear;
+//         private int front;
+
+//         Queue(int n) {
+//             arr = new int[n];
+//             size = n;
+//             rear = -1;
+//             front = -1;
+//         }
+
+//         public boolean isEmpty() {
+//             return front == -1;
+//         }
+
+//         public boolean isFull() {
+//             return (rear + 1) % size == front;
+//         }
+
+//         public void add(int data) {
+//             if (isFull()) {
+//                 System.out.println("Queue is full");
+//                 return;
+//             }
+//             if (isEmpty()) {
+//                 front = 0;
+//             }
+//             rear = (rear + 1) % size;
+//             arr[rear] = data;
+//         }
+
+//         public int remove() {
+//             if (isEmpty()) {
+//                 System.out.println("Queue is empty");
+//                 return -1;
+//             }
+//             int result = arr[front];
+//             if (front == rear) {
+//                 front = rear = -1; 
+//             } else {
+//                 front = (front + 1) % size;
+//             }
+//             return result;
+//         }
+
+//         public int peek() {
+//             if (isEmpty()) {
+//                 System.out.println("Queue is empty");
+//                 return -1;
+//             }
+//             return arr[front];
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//         Queue q = new Queue(5);
+//         q.add(1);
+//         q.add(2);
+//         q.add(3);
+//         while (!q.isEmpty()) {
+//             System.out.println(q.peek());
+//             q.remove();
+//         }
+//     }
+// }
